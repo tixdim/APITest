3d8eaa1acf8d0b13087ef8cab8d6c119412b7670
tixdim <tixdim.on1@gmail.com>
2021-09-18 17:55:29 +0300

    сделал всё

---
+++ Rubic_test/Rubic_test/Controllers/BudgetContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Rubic_test.Models;
+using System.Threading.Tasks;
+
+namespace Rubic_test.Controllers
+{
+    public class BudgetContext : DbContext
+    {
+        public BudgetContext(DbContextOptions<BudgetContext> options)
+            : base(options)
+        { 
+            
+        }
+
+        public DbSet<Budget> Budgets { get; set; }
+    }
+}
---
+++ Rubic_test/Rubic_test/Models/Budget.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rubic_test.Models
+{
+    public class Budget
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Label_percentage { get => (Percentage * 100).ToString() + "%"; }
+        public float Percentage { get; set; }
+        public string Color { get; set; }
+    }
+}
